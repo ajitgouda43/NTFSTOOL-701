79e1111f67f82e25ef519e6250a49dd28790226c
Ajit <Ajit@LAPTOP-ANFSD8KU>
2025-04-30 16:34:55 +0530

    Dev

--- GIT_NITProj01/src/main/java/com/nt/service/Arithmetic.java
+++ GIT_NITProj01/src/main/java/com/nt/service/Arithmetic.java
@@ -2,11 +2,9 @@ package com.nt.service;
 
 public class Arithmetic {
 	public double sum(double x,double y) {
-<<<<<<< HEAD
-		return (x+y);
-=======
+
 		return x+y + 0;
->>>>>>> branch 'master' of https://github.com/ajitgouda43/NTFSTOOL-701.git
+
 	}
 
 }
