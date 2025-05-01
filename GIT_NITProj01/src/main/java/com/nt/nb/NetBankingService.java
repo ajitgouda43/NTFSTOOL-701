bf97277a63cd5363dc6b751f83cd1e2fe4297178
Ajit <Ajit@LAPTOP-ANFSD8KU>
2025-05-02 00:48:18 +0530

    Tl review the netbanking

--- GIT_NITProj01/src/main/java/com/nt/nb/NetBankingService.java
+++ GIT_NITProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -2,6 +2,7 @@ package com.nt.nb;
 
 public class NetBankingService {
 	public String transfermoney(long srcAcc,long desAcc,String ifsc,double amt) {
+		//customer money transfer details
 		return amt+"money transfer from "+srcAcc+"to "+desAcc;
 	}
 }
