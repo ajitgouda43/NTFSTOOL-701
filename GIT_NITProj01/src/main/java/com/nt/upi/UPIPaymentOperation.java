05b27639285f05fda2f2613dcdb35e52a45aeb0d
Ajit <Ajit@LAPTOP-ANFSD8KU>
2025-05-02 00:14:05 +0530

    TL review the b1

--- GIT_NITProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
+++ GIT_NITProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
@@ -3,7 +3,9 @@ package com.nt.upi;
 public class UPIPaymentOperation {
 	
 	public String doPayment(long phNo,double amount) {
-		return amount+"amount is paid to phone phone number::"+phNo;
+		//upi payment Details
+		return amount+"amount is paid to  @phonenumber::"+phNo;
+		
 	}
 
 }
