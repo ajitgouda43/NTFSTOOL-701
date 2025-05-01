ebd8ea25de117adffa92fc1976ba70082da3c1fb
Ajit <Ajit@LAPTOP-ANFSD8KU>
2025-05-02 00:09:55 +0530

    Dev-1 complete the upi payment feature

---
+++ GIT_NITProj01/src/main/java/com/nt/upi/UPIPaymentOperation.java
@@ -0,0 +1,9 @@
+package com.nt.upi;
+
+public class UPIPaymentOperation {
+	
+	public String doPayment(long phNo,double amount) {
+		return amount+"amount is paid to phone phone number::"+phNo;
+	}
+
+}
